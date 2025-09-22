4121c143616a4dc6dbbd64c46a077372689fd0b7
Teeye0 <tissahazong09@gmail.com>
2025-09-22 16:35:16 +0530

    packagejson files and related

---
+++ math.js
@@ -0,0 +1,14 @@
+/*module.exports.sum= (a,b)=> a+b;
+const mul=(a,b)=> a*b;
+const pi=3.14;
+module.exports.g=5.5;
+
+module.exports={
+    mul:mul,
+    pi:pi,
+}*/
+
+//module.exports=obj;
+
+export const sum=(a,b)=>a+b;
+export const pi=3.14;
---
+++ script.js
@@ -0,0 +1,86 @@
+//import{sum,pi} from "./math.js";
+import { generate } from "random-words";
+
+//console.log(sum(2,2));
+//console.log(pi);
+console.log(generate());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let args=process.argv;
+for(let i=2;i<args.length;i++){
+    console.log("hello,", args[i]);
+}
+
+const values=require("./math");
+console.log(values);
+console.log(values.pi);
+console.log(values.mul(2,2));
+
+const info=require("./fruits");
+console.log(info);
+console.log(info[1]);
+console.log(info[0].color);
+console.log(info[2].name);*/
+
+/*const figlet=require("figlet");
+
+figlet("TISSAHAZONG!!", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});*/
+
